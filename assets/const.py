eb10cb37d59511e881b6ae7e035fbc5afee7c2f6
Max <max.arnouts@ictmbo.nl>
2022-06-14 15:34:05 +0200

    Added documentation

--- assets/const.py
+++ assets/const.py
@@ -1,12 +1,14 @@
+# Data for password generation.
 lower_case = "abcdefghijklmnopqrstuvwxyz"
 upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 numbers = "0123456789"
 special_characters = "!@#$%^&*()_+"
 
+# Option if default is not picked.
 options = """
-        Pick one of the following numbers:
-        1. Lowercase/Uppercase characters.
-        2. Uppercase/Numbers characters.
-        3. Numbers/Special characters.
-        4. Special/Lowercase characters.
+Pick one of the following numbers:
+1. Lowercase/Uppercase characters.
+2. Uppercase/Numbers characters.
+3. Numbers/Special characters.
+4. Special/Lowercase characters.
         """
